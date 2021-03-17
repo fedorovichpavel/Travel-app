3fb471f3f8954dc22ca2ffe72325dcefd1735ddb
Maryna Migacheva <m.migacheva@gmail.com>
2021-03-17 12:26:49 +0300

    feat: add modal for item gallery

--- src/components/gallery/gallery.tsx
+++ src/components/gallery/gallery.tsx
@@ -44,14 +44,12 @@ function Gallery() {
       title: 'Galley of type',
       preview: 'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.'
     }
-  ]
+  ];
 
   return (
     <div className="gallery">
       <div className="wrap-gallery">
-        <ul>
           <ItemGallery list={list} />
-        </ul>
       </div>
     </div>
   )
