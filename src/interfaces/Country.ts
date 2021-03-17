336b6ce869659dc6f81c8ae57532550c141eb67e
Pavel Fedarovich <fedorovich.pavel@mail.ru>
2021-03-17 13:18:16 +0300

    feat:add config

--- src/interfaces/Country.ts
+++ src/interfaces/Country.ts
@@ -18,5 +18,5 @@ export interface WeatherI {
 }
 
 export interface ExchangeI {
-  rates: object,
+  rates: object
 }
--- src/redux/reducer/langReducer.ts
+++ src/redux/reducer/langReducer.ts
@@ -1,12 +1,13 @@
 import { LANG } from "../actionTypes";
 
-const initialState = {
-  lang: "en",
+const initialState:any = {
+  lang: localStorage.getItem("language") ? localStorage.getItem("language") : "en",
 };
 
 export const langReducer = (state = initialState, action: any) => {
   switch (action.type) {
     case LANG:
+      localStorage.setItem("language", action.payload);
       return { ...state, lang: action.payload };
     default:
       return state;
