8b38aab54d1ba1fc705d27d64e44c92b783519d1
Mikhail Hursky <kakai747o@gmail.com>
2021-03-17 17:29:58 +0300

    refactor: delete unused varible

--- src/components/gallery/gallery.tsx
+++ src/components/gallery/gallery.tsx
@@ -6,58 +6,7 @@ import "./../../Normalize.css";
 import "./gallery.scss";
 
 function Gallery(props: any) {
-  console.log(props.images);
-  const list = [
-    {
-      id: 1,
-      src:
-        "https://travelata.ru/blog/wp-content/uploads/2019/06/s1200-12-1024x640.jpg",
-      title: "Galley of type",
-      preview:
-        "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-    },
-    {
-      id: 2,
-      src:
-        "https://travelata.ru/blog/wp-content/uploads/2019/06/s1200-12-1024x640.jpg",
-      title: "Galley of type",
-      preview:
-        "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-    },
-    {
-      id: 3,
-      src:
-        "https://travelata.ru/blog/wp-content/uploads/2019/06/s1200-12-1024x640.jpg",
-      title: "Galley of type",
-      preview:
-        "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-    },
-    {
-      id: 4,
-      src:
-        "https://travelata.ru/blog/wp-content/uploads/2019/06/s1200-12-1024x640.jpg",
-      title: "Galley of type",
-      preview:
-        "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-    },
-    {
-      id: 5,
-      src:
-        "https://travelata.ru/blog/wp-content/uploads/2019/06/s1200-12-1024x640.jpg",
-      title: "Galley of type",
-      preview:
-        "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-    },
-    {
-      id: 6,
-      src:
-        "https://travelata.ru/blog/wp-content/uploads/2019/06/s1200-12-1024x640.jpg",
-      title: "Galley of type",
-      preview:
-        "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-    },
-  ];
-  const list2 = props.images.map((image: any) => ({
+  const list = props.images.map((image: any) => ({
     id: image._id,
     src: image.img,
     title: {
@@ -76,7 +25,7 @@ function Gallery(props: any) {
   return (
     <div className="gallery">
       <div className="wrap-gallery">
-        <ItemGallery list={list2} />
+        <ItemGallery list={list} />
       </div>
     </div>
   );
